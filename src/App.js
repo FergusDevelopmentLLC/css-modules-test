e909c6704a10923545c91029f443c6f570a398e2
Will Carter <will.carter@fergusllc.com>
2019-12-15 20:49:27 -0700

    added documentation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./App.module.css";
 import Paragraph from "./Paragraph";
+import source from './source.png';
+
 const App = () => {
   return (
     <div>
       <p className={styles.red}>A class named "red" is defined in App.modules.css. App.module.css is imported in App.js. I am red because I am styled like: {`<p `}className={`{styles.red}`}>content{`</p>`}.</p>
       <p className="red">I don't pick up the red class from App.module.css even though my class is "red", which is correct. I am styled like: {`<p `}className={`"red"`}>content{`</p>`}.</p>
       <Paragraph />
+      <p>Documented: <a href='https://create-react-app.dev/docs/adding-a-css-modules-stylesheet'>https://create-react-app.dev/docs/adding-a-css-modules-stylesheet</a></p>
+      <img src={source} />
     </div>
   );
 };
