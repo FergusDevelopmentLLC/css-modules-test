711906d33b7accd7fe66d248ca8d8bbe7c33546d
Will Carter <will.carter@fergusllc.com>
2019-12-15 20:39:52 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import styles from "./App.module.css";
+import Paragraph from "./Paragraph";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className={styles.red}>A class named "red" is defined in App.modules.css. App.module.css is imported in App.js. I am red because I am styled like: {`<p `}className={`{styles.red}`}>content{`</p>`}.</p>
+      <p className="red">I don't pick up the red class from App.module.css even though my class is "red", which is correct. I am styled like: {`<p `}className={`"red"`}>content{`</p>`}.</p>
+      <Paragraph />
     </div>
   );
-}
-
+};
 export default App;
